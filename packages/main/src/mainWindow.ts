39b605888057943afe832c04c263af619a95025d
SimonBerens <sberensnyc@gmail.com>
2022-02-27 15:32:32 -0500

    Get electron-store working (jesus christ)

--- packages/main/src/mainWindow.ts
+++ packages/main/src/mainWindow.ts
@@ -5,7 +5,9 @@ import {URL} from 'url';
 async function createWindow() {
   const browserWindow = new BrowserWindow({
     frame: false,
+    height: 150,
     show: false, // Use 'ready-to-show' event to show window
+    width: 450,
     webPreferences: {
       nativeWindowOpen: true,
       webviewTag: false, // The webview tag is not recommended. Consider alternatives like iframe or Electron's BrowserView. https://www.electronjs.org/docs/latest/api/webview-tag#warning
--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,9 +1,7 @@
 <script lang="ts" setup>
-import ReactiveCounter from '/@/components/ReactiveCounter.vue';
-import ReactiveHash from '/@/components/ReactiveHash.vue';
-import ElectronVersions from '/@/components/ElectronVersions.vue';
 import RunButton from '/@/components/RunButton.vue';
 import './index.css';
+import Settings from '/@/components/Settings.vue';
 </script>
 
 <template>
@@ -13,33 +11,6 @@ import './index.css';
     width="150"
   >
 
+  <settings />
   <run-button />
-  <p>
-    For a guide and recipes on how to configure / customize this project,<br>
-    check out the
-    <a
-      href="https://github.com/cawa-93/vite-electron-builder"
-      target="_blank"
-    >vite-electron-builder documentation</a>.
-  </p>
-
-  <fieldset>
-    <legend>Test Vue Reactivity</legend>
-    <reactive-counter />
-  </fieldset>
-
-  <fieldset>
-    <legend>Test Node.js API</legend>
-    <reactive-hash />
-  </fieldset>
-
-  <fieldset>
-    <legend>Environment</legend>
-    <electron-versions />
-  </fieldset>
-
-  <p>
-    Edit
-    <code>packages/renderer/src/App.vue</code> to test hot module replacement.
-  </p>
 </template>
